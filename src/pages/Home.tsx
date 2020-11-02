6381e666e13d5910cb3c8871a57a68b4dac551c2
Veigar <1164358872@qq.com>
2020-11-02 10:38:18 +0800

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,168 +1,69 @@
 import React from "react";
 import { Card } from 'antd';
-import styles from './Welcome.less';
+import styles from './home.less';
+import classNames from "classnames/bind";
 
+const cs = classNames.bind(styles);
 export default class Drag1 extends React.Component {
 
-  componentDidMount(): void {
-
-    let cvs = document.querySelector('canvas');
-    let ctx = cvs.getContext('2d');
-    let meteorShower = new MeteorShower(cvs, ctx);
-    meteorShower.start();
-  }
-
   render() {
+    const spaces = (index: number, size: string ) =>cs({
+      'space__star': true,
+      [`space__star--${index}`]: true,
+      [`space__star--${size}`]: true
+    });
+    const spaceShooting = cs({
+      'space__star': true,
+      'space__star--shooting': true
+    });
+    const sizes = ['small','small','small','medium','small','small','big','medium','small','small','small','medium','small','big','small','medium','small','small','small',
+    'medium','big','small','small',',medium','small','small','small','medium','small'];
+    const loves = (index: number) =>cs({
+      'love__heart': true,
+      [`love__heart--${index}`]: true
+    });
+    const moons = (index: number, size: string ) =>cs({
+      'moon__crater': true,
+      [`moon__crater--${index}`]: true,
+      [`moon__crater--${size}`]: true
+    });
+    const moonCustom = (a: string, b: string)  =>cs({
+      [`${a}`]: true,
+      [`${b}`]: true,
+    });
     return (
-      <Card className={styles.homeCard}>
-        <div className={styles.homeCircle}>
-          <div/><div/><div/>
-          <div style={{position: 'absolute',color:'#fff',top:90,left:100,fontSize:60}}>
-            LOL
+      <div className={styles.spinSty}>
+        <div className={styles.space}>
+          <div className={spaceShooting} />
+          {
+            sizes.map((size,index)=>
+              <div key={`space${index+1}`} className={spaces(index+1,size)} />
+            )
+          }
+        </div>
+        <div className={styles.moon}>
+          <div className={styles.love}>
+            <div className={loves(1)} />
+            <div className={loves(2)} />
+            <div className={loves(3)} />
+          </div>
+          <div className={styles.moon__container}>
+            <div className={moons(1,'small')} />
+            <div className={moons(2,'small')} />
+            <div className={moons(3,'small')} />
+            <div className={moons(4,'medium')} />
+            <div className={moons(5,'medium')} />
+            <div className={moons(6,'big')} />
+            <div className={styles.moon__face}>
+              <div className={moonCustom('moon__eye','moon__eye--left')} />
+              <div className={moonCustom('moon__eye','moon__eye--right')} />
+              <div className={moonCustom('moon__cheek','moon__cheek--left')} />
+              <div className={moonCustom('oon__cheek','moon__cheek--right')} />
+              <div className={styles['moon__smile']} />
+            </div>
           </div>
         </div>
-        <canvas style={{height:'100%',width:'100%'}} />
-      </Card>
+      </div>
     )
-  }
-}
-
-// 坐标
-class Crood {
-  constructor(x=0, y=0) {
-    this.x = x;
-    this.y = y;
-  }
-  setCrood(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-  copy() {
-    return new Crood(this.x, this.y);
-  }
-}
-
-// 流星
-class ShootingStar {
-  constructor(init=new Crood, final=new Crood, size=0.0001, speed=50, onDistory=null) {
-    this.init = init; // 初始位置
-    this.final = final; // 最终位置
-    this.size = size; // 大小
-    this.speed = speed; // 速度：像素/s
-
-    // 飞行总时间
-    this.dur = Math.sqrt(Math.pow(this.final.x-this.init.x, 2) + Math.pow(this.final.y-this.init.y, 2)) * 1000 / this.speed;
-
-    this.pass = 0; // 已过去的时间
-    this.prev = this.init.copy(); // 上一帧位置
-    this.now = this.init.copy(); // 当前位置
-    this.onDistory = onDistory;
-  }
-  draw(ctx, delta) {
-    this.pass += delta;
-    this.pass = Math.min(this.pass, this.dur);
-
-    let percent = this.pass / this.dur;
-
-    this.now.setCrood(
-      this.init.x + (this.final.x - this.init.x) * percent,
-      this.init.y + (this.final.y - this.init.y) * percent
-    );
-
-    // canvas
-    ctx.strokeStyle = '#fff';
-    ctx.lineCap = 'round';
-    ctx.lineWidth = this.size;
-    ctx.beginPath();
-    ctx.moveTo(this.now.x, this.now.y);
-    ctx.lineTo(this.prev.x, this.prev.y);
-    ctx.stroke();
-
-    this.prev.setCrood(this.now.x, this.now.y);
-    if (this.pass === this.dur) {
-      this.distory();
-    }
-  }
-  distory() {
-    this.onDistory && this.onDistory();
-  }
-}
-
-class MeteorShower {
-
-  constructor(cvs, ctx) {
-    this.cvs = cvs;
-    this.ctx = ctx;
-    this.stars = [];
-    this.T;
-    this.stop = false;
-    this.playing = false;
-  }
-
-  createStar() {
-    let angle = Math.PI / 3;
-    let distance = Math.random() * 400;
-    let init = new Crood(Math.random() * this.cvs.width|0, Math.random() * 100|0);
-    let final = new Crood(init.x + distance * Math.cos(angle), init.y + distance * Math.sin(angle));
-    let size = Math.random() * 2 * 0.5;
-    let speed = Math.random() * 400;
-    let star = new ShootingStar(
-      init, final, size, speed,
-      ()=>{this.remove(star)}
-    );
-    return star;
-  }
-
-  remove(star) {
-    this.stars = this.stars.filter((s)=>{ return s !== star});
-  }
-
-  update(delta) {
-    if (!this.stop && this.stars.length < 20) {
-      this.stars.push(this.createStar());
-    }
-    this.stars.forEach((star)=>{
-      star.draw(this.ctx, delta);
-    });
-  }
-
-  tick() {
-    if (this.playing) return;
-    this.playing = true;
-
-    let now = (new Date()).getTime();
-    let last = now;
-    let delta;
-
-    let  _tick = ()=>{
-      if (this.stop && this.stars.length === 0) {
-        cancelAnimationFrame(this.T);
-        this.playing = false;
-        return;
-      }
-
-      delta = now - last;
-      delta = delta > 500 ? 30 : (delta < 16? 16 : delta);
-      last = now;
-      // console.log(delta);
-
-      this.T = requestAnimationFrame(_tick);
-
-      this.ctx.save();
-      this.ctx.fillStyle = 'rgba(0,0,0,0.2)'; // 每一帧用 “半透明” 的背景色清除画布
-      this.ctx.fillRect(0, 0, this.cvs.width, this.cvs.height);
-      this.ctx.restore();
-      this.update(delta);
-    }
-    _tick();
-  }
-
-  start() {
-    this.stop = false;
-    this.tick();
-  }
-
-  stop() {
-    this.stop = true;
   }
 }
